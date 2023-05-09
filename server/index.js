78510bfd1fdfd5bf0bcbbc4b2d934891e9583009
malaksboui <sboui.95malak@yahoo.com>
2023-05-09 18:08:50 +0100

    databasedone

--- server/index.js
+++ server/index.js
@@ -1,2 +1,34 @@
 
 
+const express = require("express");
+const app = express();
+const PORT = 4000;
+const cors = require("cors");
+const db= require("../server/database.mysql/index")
+
+app.use(cors())
+app.use(express.json())
+
+
+  app.get("/get", (req, res) => {
+  
+    db.query("SELECT * FROM songs", (err, data) => {
+      if (err) {
+        console.log(err);
+         res.send("err")
+      }
+      else{
+      return res.send(data);
+      }
+    });
+  });
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
+
+
